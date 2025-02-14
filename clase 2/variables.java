57d7208cc2a89002a075bce3206c2b1d899e6227
Herrera914 <148608297+Herrera914@users.noreply.github.com>
2025-02-14 01:22:59 +0000

    Ejercicios clase 2

---
+++ clase 2/variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    //Método main
+    public static void main (String[] args){
+        
+        // tipos de datos
+        // Enteros int, long
+        int edad = 25;
+        // Reales double, float
+        double altura = 1.75; 
+        //Cadenas
+        String nombre = "Juan";
+        //Lógicos
+        boolean esEstudiante = true; //false  
+        System.out.println("Nombre: " + nombre + ",Edad: " + edad + ", Altura: " + altura 
+        +  ", Estudiante: " + esEstudiante );             
+    }
+}
