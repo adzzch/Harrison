ae4af9e588011b3169020cc5bdbad4e43baa6771
Herrera914 <148608297+Herrera914@users.noreply.github.com>
2025-02-28 01:08:12 +0000

    Creacion de los objetos

---
+++ clase4/Ejecutar.java
@@ -0,0 +1,23 @@
+package clase4;
+
+public class Ejecutar {
+    public static void main(String[] args) {
+        //String va en comillas
+        //Forma1
+        Vehiculo objVehiculo1 = new Vehiculo("Toyota", "Blanco", 4500.0, "TSKFld", 2.3, "150HP");
+        
+        //Forma
+        Vehiculo objVehiculo2;
+        objVehiculo2= new Vehiculo("Ferrari", "Rojo", 5000.0,"iudfhoisu", 1.8, "1300hp");
+
+     //MOstrar la marca  del objeto vehiculo 1
+        System.out.println(objVehiculo1.getMarca());
+
+        //Se va a modificar la marca del vehiculo del objeto objVehiculo1
+        objVehiculo1.setMarca("Chevrolet");
+
+        //Mostrar todo objeto
+        System.out.println(objVehiculo1.toString());
+        System.out.println(objVehiculo2);
+    }
+}
